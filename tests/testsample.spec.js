969a0b16390ddce488ce697c596e43219e455f3e
gb@rocketsoftware.com <gb@rocketsoftware.com>
2025-02-05 02:21:47 -0500

    ORMF - 3348 and ORMF -3349

--- tests/testsample.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-
-test('sample check', async ({page}) => {
-    await page.goto("https://www.google.co.uk/");
-    await page.goto("https://lnxrrmf2:3010/login");   
-});
